0e294b87d9a560688e37bdafcba08e0ddcddb95c
윤유림 <0232dbfla@naver.com>
2023-10-05 14:25:31 +0900

    시야 수정, door action 스크립트 캐릭터 lookAt에 추가

--- Assets/Enviroment/enviroment/Office building/scripts/DoorAction.cs
+++ Assets/Enviroment/enviroment/Office building/scripts/DoorAction.cs
@@ -1,36 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class DoorAction : MonoBehaviour {
-
-
-   
-
-   
-
+public class DoorAction : MonoBehaviour 
+{
     void Update ()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-          
+            Debug.Log("key down e");
             RaycastHit hit;
-
             Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward), out hit);
-            
-                
-                if (hit.transform.tag == "door")
-                {
-
+            Debug.Log(hit.transform.tag);
+            if (hit.transform.tag == "door")
+            {
                 hit.transform.gameObject.GetComponent<Door>().ActionDoor();
-
-
-                }
-
-                if(hit.collider.gameObject.name == "Button floor 1")
-                {
+            }
+            if(hit.collider.gameObject.name == "Button floor 1")
+            {
                 hit.transform.gameObject.GetComponent<pass_on_parent>().MyParent.GetComponent<evelator_controll>().AddTaskEve("Button floor 1");
-
             }
             if (hit.collider.gameObject.name == "Button floor 2")
             {
@@ -52,11 +41,6 @@ public class DoorAction : MonoBehaviour {
             {
                 hit.transform.gameObject.GetComponent<pass_on_parent>().MyParent.GetComponent<evelator_controll>().AddTaskEve("Button floor 6");
             }
-
-
-
         }
-
-		
 	}
 }
